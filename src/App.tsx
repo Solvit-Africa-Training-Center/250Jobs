8fa7f0721dc9b184d8551d691a4407700e5028b9
Belyse Ingabire <your-email@example.com>
2025-09-10 11:36:22 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className='bg-amber-500'>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. A corporis, inventore repudiandae odio facere temporibus maiores impedit accusantium quibusdam architecto molestias ea aliquam similique consectetur ullam iusto amet repellat ex?
+    </div>
+  )
+}
+
+export default App
