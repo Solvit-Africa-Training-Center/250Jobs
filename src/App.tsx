711a94a51332de046b4aa7685dee9f4e644da2a9
Belyse Ingabire <your-email@example.com>
2025-09-10 12:36:43 +0200

    installed tools

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@
 function App() {
   return (
     <div className='bg-amber-500'>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. A corporis, inventore repudiandae odio facere temporibus maiores impedit accusantium quibusdam architecto molestias ea aliquam similique consectetur ullam iusto amet repellat ex?
-    </div>
+      
+      </div>
   )
 }
 
